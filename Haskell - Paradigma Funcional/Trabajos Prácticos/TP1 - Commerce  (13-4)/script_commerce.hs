5dcd17e95bce1e3cb40a4a351551363a9a29decc
Martin Caruso (Desktop) <martinpepa39@gmail.com>
2020-04-24 22:18:44 -0300

    TP1: Commerce Version Final

--- Haskell - Paradigma Funcional/Trabajos Prácticos/TP1 - Commerce  (13-4)/script_commerce.hs
+++ Haskell - Paradigma Funcional/Trabajos Prácticos/TP1 - Commerce  (13-4)/script_commerce.hs
@@ -1,5 +1,4 @@
 -- Trabajo Prácrico 1: Commerce - Paradigmas de Programacion
--- Fecha de entrega: ??/04
 -- Hecho por Martín Caruso
 
 productoCorriente :: String -> Bool 
@@ -11,38 +10,81 @@ productoCorriente nombre
     | head nombre == 'U' || head nombre == 'u' = True
     | otherwise = False
 
+-- productoCorriente "Ariel"      -> True
+-- productoCorriente "escuadra"   -> True
+-- productoCorriente "Iman"       -> True
+-- productoCorriente "olla"       -> True
+-- productoCorriente "Ukelele     -> True
+-- productoCorriente "Heladera"   -> False
+
 productoXL :: String -> String 
 productoXL nombre = nombre ++ " XL"
   
+-- productoXL "Heladera"    -> "Heladera XL"
+
 productoCodiciado :: String -> Bool 
-productoCodiciado nombre = length nombre >= 10
+productoCodiciado nombre = length nombre > 10
+
+-- productoCodiciado "Heladera XL"  -> True
+-- productoCodiciado "Heladera"     -> False
+-- productoCodiciado "Heladera Z"   -> False
 
 descodiciarProducto :: String -> String 
 descodiciarProducto nombre 
     | productoCodiciado nombre = take 10 nombre
     | otherwise = nombre 
 
+-- descodiciarProducto "Heladera XL"  -> "Heladera X"
+-- descodiciarProducto "Heladera"     -> "Heladera"   
+-- descodiciarProducto "Heladera Z"   -> "Heladera Z" 
+
 aplicarCostoEnvio :: Float -> Float -> Float
 aplicarCostoEnvio precio costoEnvio = precio + costoEnvio 
+
+-- aplicarCostoEnvio 10.1 5.2   -> 15.3
 
 entregaSencilla :: String -> Bool
 entregaSencilla = even.length  
 
+-- entregaSencilla "Heladera XL"  -> False
+-- entregaSencilla "Heladera"     -> True
+-- entregaSencilla "Heladera Z"   -> True
+
 versionBarata :: String -> String
 versionBarata = reverse.descodiciarProducto
 
+-- versionBarata "Heladera XL"  -> "LX aredaleH"
+-- versionBarata "Heladera"     -> "aredaleH"   
+-- versionBarata "Heladera Z"   -> "Z aredaleH" 
+
 productoDeLujo :: String -> Bool
-productoDeLujo nombre = elem 'x' nombre || elem 'z' nombre
+productoDeLujo nombre = elem 'x' nombre || elem 'z' nombre || elem 'X' nombre || elem 'Z' nombre
+
+-- productoDeLujo "Heladera XL"  -> True
+-- productoDeLujo "Heladera"     -> False
+-- productoDeLujo "Heladera Z"   -> True
 
 aplicarDescuento :: Float -> Float -> Float
 aplicarDescuento precio descuento = precio - precio * descuento / 100  
 
+-- aplicarDescuento 300.0 15.0  -> 255.0
+
 productoDeElite :: String -> Bool
 productoDeElite nombre = productoDeLujo nombre && productoCodiciado nombre && not (productoCorriente nombre)
 
---Reemplazar Float con Int funciona 
+-- productoDeElite "Pad Pro Mini XL"    -> True
+-- productoDeElite "iPad Pro Mini XL"   -> False
+
+-- productoDeElite "Heladera XL"        -> True
+-- productoDeElite "Heladera X"         -> False
+
+-- productoDeElite "Sillon verde XL"    -> True
+-- productoDeElite "Sillon verde SL"    -> False
+
 precioTotal :: Int -> Float -> Float -> Float -> Float
 precioTotal cantidad precio descuento costoEnvio = aplicarCostoEnvio (aplicarDescuento precio descuento * fromIntegral(cantidad)) costoEnvio   
+
+-- precioTotal 10 200.5 15.3 400.99 -> 2099.225
 
 --FUNCIONES UTILES (SOLO TIPADAS)
 --take :: Int -> [a] -> [a]
@@ -51,6 +93,3 @@ precioTotal cantidad precio descuento costoEnvio = aplicarCostoEnvio (aplicarDes
 --elem :: Eq => a -> [a] -> Bool
 --reverse :: [a] -> [a]
 --[a] se refiere a listas, como String o una lista de numeros
-
---DUDAS
---  Es necesario que definamos las funciones del final, o las dejamos como comentarios?
